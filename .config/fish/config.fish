995f0830275ee127bde67e4f41567553a04152b5
_Bare <=>
2021-02-12 19:09:28 -0500

    Fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,31 @@
+#//----------[ Title ]
+function fish_title
+	echo $_ ' '
+	pwd
+end
+
+#//----------[ Greeting ]
+function fish_greeting
+		printf  '\e[90m%s\n%s\n%s\n%s\n' "                 |               !!!           |\"|      " "     ,,,         |.===.       \  _ _  /       _|_|_     " "    (o o)        {}o o{}     -  (OXO)  -      (o o)     " "ooO--(_)--Ooo-ooO--(_)--Ooo-ooO--(_)--Ooo-ooO--(_)--Ooo-"
+end
+
+#//----------[ Alias ]
+# list aliases for Exa
+alias l="exa -Fx --group-directories-first"
+alias lh="exa -Fax --group-directories-first"
+alias ll="exa -lF --group-directories-first"
+alias la="exa -lFa --group-directories-first"
+# exit
+alias x="exit"
+# clear screen
+alias cls="clear"
+# poweroff
+alias die="poweroff"
+# git dotfiles
+alias config="/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME"
+
+#//----------[ PATH ]
+set -gx PATH ~/.cargo/bin $PATH
+
+#//----------[ STARSHIP ]
+starship init fish | source
