4fef7bf3b1fa836c3089289ceb831a42b775bf58
_Bare <bare@logixsr.com>
2021-05-03 08:37:38 -0400

    recursion, typer, break, tv fn's, c920

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -36,7 +36,7 @@ end
 alias todo="nvim ~/code/bin/twitchy/twitchy.todo"
 # nvim
 alias nm="nvim src/main.rs"
-alias nr="nvim src/*"
+alias nr="nvim src/**/*.rs"
 alias n="nvim"
 # bat
 alias cat bat
@@ -47,7 +47,39 @@ alias du dust
 # fend
 alias calc fend
 # wallpaper random
-alias wall "feh --randomize --bg-scale ~/Downloads/wallpapers/"
+alias wall "feh -r --randomize --bg-scale ~/Downloads/wallpapers/"
+# config webcam defaults
+alias c920 "v4l2-ctl -d /dev/video-c920 -c white_balance_temperature_auto=0 -c gain=128 -c white_balance_temperature=4225 -c sharpness=165 -c exposure_auto=1 -c exposure_absolute=200 -c focus_auto=0 -c exposure_auto_priority=0 -c saturation=200"
+# web stuff
+alias web "firefox & disown"
+alias email "thunderbird & disown"
+alias tor "qbittorrent & disown"
+
+# //----------[ VLC Media Library Stuff ]
+function tv
+	switch (string lower $argv)
+		case "g" or "get"
+			cp ~/mnt/C/Users/barek/AppData/Roaming/vlc/ml.xspf ~/.local/share/vlc/
+			cp ~/mnt/C/Users/barek/AppData/Roaming/vlc/ml.xspf ~/mnt/C/Users/barek/AppData/Roaming/vlc/ml.xspf.backup
+			sed -i 's/D:\//home\/bare\/mnt\/D\//g' ~/.local/share/vlc/ml.xspf
+			sed -i 's/C:\//home\/bare\/mnt\/C\//g' ~/.local/share/vlc/ml.xspf
+		case "p" or "put" or "s" or "set"
+			cp ~/.local/share/vlc/ml.xspf  ~/mnt/C/Users/barek/AppData/Roaming/vlc/
+			sed -i 's/home\/bare\/mnt\/D\//D:\//g' ~/mnt/C/Users/barek/AppData/Roaming/vlc/ml.xspf 
+			sed -i 's/home\/bare\/mnt\/C\//C:\//g' ~/mnt/C/Users/barek/AppData/Roaming/vlc/ml.xspf 
+	end
+end
+
+#//----------[ TYPER ]
+function typer
+	cd ~/code/rust/toggle_cool_cow_says_type
+    cargo r -- -w 8 ./src
+end
+
+# //----------[ Break ]
+function Break
+	afk "Break" -m 3 -c purple
+end
 
 #//----------[ FZF ]
 begin
