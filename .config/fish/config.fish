2a95212c15d92f812db971cc1b02afdec45d465a
_Bare <bare@logixsr.com>
2021-05-06 13:26:21 -0400

    cursor size; tabs; feh; tv

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -46,14 +46,19 @@ alias ps procs
 alias du dust
 # fend
 alias calc fend
-# wallpaper random
-alias wall "feh -r --randomize --bg-scale ~/Downloads/wallpapers/"
 # config webcam defaults
 alias c920 "v4l2-ctl -d /dev/video-c920 -c white_balance_temperature_auto=0 -c gain=128 -c white_balance_temperature=4225 -c sharpness=165 -c exposure_auto=1 -c exposure_absolute=200 -c focus_auto=0 -c exposure_auto_priority=0 -c saturation=200"
 # web stuff
 alias web "firefox & disown"
 alias email "thunderbird & disown"
 alias tor "qbittorrent & disown"
+
+# //----------[ feh ]
+alias wall "do_feh -r --randomize --bg-scale ~/Downloads/wallpapers/"
+alias show "do_feh --randomize -z -D 4"
+function do_feh
+	feh -.Z -B "#080808" $argv & disown
+end
 
 # //----------[ VLC Media Library Stuff ]
 function tv
@@ -65,15 +70,17 @@ function tv
 			sed -i 's/C:\//home\/bare\/mnt\/C\//g' ~/.local/share/vlc/ml.xspf
 		case "p" or "put" or "s" or "set"
 			cp ~/.local/share/vlc/ml.xspf  ~/mnt/C/Users/barek/AppData/Roaming/vlc/
-			sed -i 's/home\/bare\/mnt\/D\//D:\//g' ~/mnt/C/Users/barek/AppData/Roaming/vlc/ml.xspf 
-			sed -i 's/home\/bare\/mnt\/C\//C:\//g' ~/mnt/C/Users/barek/AppData/Roaming/vlc/ml.xspf 
+			sed -i 's/home\/bare\/mnt\/D\//D:\//g' ~/mnt/C/Users/barek/AppData/Roaming/vlc/ml.xspf
+			sed -i 's/home\/bare\/mnt\/C\//C:\//g' ~/mnt/C/Users/barek/AppData/Roaming/vlc/ml.xspf
+		case "c" or "clean"
+			rm -v ~/mnt/D/TV/**/*.exe ~/mnt/D/TV/**/*.txt ~/mnt/D/TV/**/*.nfo
 	end
 end
 
 #//----------[ TYPER ]
 function typer
 	cd ~/code/rust/toggle_cool_cow_says_type
-    cargo r -- -w 8 ./src
+	cargo r -- -w 8 ./src
 end
 
 # //----------[ Break ]
@@ -103,6 +110,7 @@ end
 
 #//----------[ VARS ]
 set -x XDG_CONFIG_HOME ~/.config
+set -x QT_QPA_PLATFORMTHEME 'qt5ct'
 
 #//----------[ PATH ]
 set -x PATH ~/.cargo/bin ~/.local/bin $PATH ~/.zls/x86_64-linux ~/code/bin/
